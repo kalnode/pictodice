8c3934f4703eb83a03b2e392f04eee788ba757a5
Kal <kounapuu@gmail.com>
2023-05-12 11:21:10 -0400

    New logo; new home layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,41 @@
 <script setup>
+import { ref } from 'vue'
 import { usePictodiceAppStore } from '@/stores/app'
 const store = usePictodiceAppStore()
 const config = useRuntimeConfig()
+const showPresetSection = ref(false)
 definePageMeta({
     title: 'Pictodice'
 })
+
 </script>
 <template>
     <div id="indexWrapper" class="w-full h-full app-padding-x flex flex-col items-center overflow-auto">
 
-        <div class="h-1/3 p-8 flex justify-center items-center">
-            <Logo tabindex=0 />
-        </div>
-
         <nav class="w-full max-w-4xl pb-48">
-            <div class="w-full flex flex-col sm:flex-row sm:justify-center">
-                <!-- LEFT -->
-                <section v-motion-slide-left :delay="700" class="card card_padding flex-1 flex justify-end items-center mb-4 sm:mr-4">
+            <div class="w-full flex flex-col sm:flex-row sm:justify-center my-4 sm:mt-28">
+
+                <section class="relative flex-1 flex justify-center sm:justify-start items-center">
+                    <div v-motion
+                        :initial="{ opacity: 0, x: -300 }"
+                        :enter="{ opacity: 1, x: 0, transition: { duration: 400 } }"
+                        class="p-4 flex justify-center items-center">
+                            <Logo tabindex=0 class="w-48 h-auto" />
+                    </div>
+                    <div class="hidden sm:block absolute right-10 rotate-12 opacity-30 w-1 h-full border-r border-[color:var(--color-tertiary)]"> </div>
+                </section>
+
+               <section  v-motion
+                    :initial="{ opacity: 0, x: 300, }"
+                    :enter="{ opacity: 1, x: 0, transition: { duration: 400, delay: 150 } }"
+                    class="card_padding flex-1 flex flex-col justify-center items-center mb-4 sm:mr-4">
 
                     <!-- TODO: Ideally we want to use a named route with params, but apparently that is ill-advised, and now throws an error (from VueRouter) -->
                     <!-- <NuxtLink :to="{ name: 'Roll', params: { 'set': 'classic' } }" class="flex flex-col items-center"> -->
                     <!-- SEE: https://github.com/vuejs/router/blob/main/packages/router/CHANGELOG.md#414-2022-08-22 -->
                     <!-- FOR NOW: This works, we go to a raw path: -->
-                    <NuxtLink to="/roll?classic" class="flex-1 flex flex-col items-center" tabindex=-1>
-                        <div class="flex-1 mb-4 flex transition hover:scale-105 space-x-4 p-8 pb-8 sm:p-12">
+                    <NuxtLink to="/roll?classic" class="max-w-xs flex-1 flex flex-col items-center" tabindex=-1>
+                        <div class="w-full flex-1 mb-8 flex transition hover:scale-105" style="max-width: 200px">
                             <!--
                             <IconsBase name="die" class="w-12 h-auto" />
                             <IconsBase name="die" class="w-12 h-auto" />
@@ -31,14 +43,32 @@ definePageMeta({
                             <img :src="config.app.baseURL+'images/dice_screenshots/test1.png'" class="dice_preview object-contain group-focus:scale-105 group-active:scale-105 group-hover:scale-105 transition-transform" />
                         </div>
                     </NuxtLink>
-                        <NuxtLink to="/roll?classic" class="btnapp btn_large hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
-                            Roll Classic
-                        </NuxtLink>
+                    <NuxtLink to="/roll?classic" class="btnapp btn_small hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
+                        Roll Classic
+                    </NuxtLink>
                 </section>
-                <!-- RIGHT -->
-                <section v-motion-slide-right :delay="500" class="card card_padding flex-1 flex justify-end items-center mb-4">
+            </div>
+
+            <div class="w-full flex flex-col sm:flex-row sm:justify-center">
+
+                <section v-motion
+                    :initial="{ opacity: 0, y: 400}"
+                    :enter="{ opacity: 1, y: 0, transition: { duration: 400, delay: 300 } }" class="flex-1 card card_padding flex justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+                    <NuxtLink :to="{ name: 'Sets'}" class="flex-1 flex items-center" tabindex=-1>
+                        <div class="w-full mb-2 flex justify-center transition hover:scale-105 space-x-4 p-4" style="max-width: 150px">
+                            <IconsBase name="pencil" class="w-full h-auto" />
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink :to="{ name: 'Sets'}" class="btnapp btn_small hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
+                        Create Custom Dice
+                    </NuxtLink>
+                    <span class="text-xs opacity-90 mt-2">(coming soon)</span>
+                </section>
+
+                <section v-if="showPresetSection" v-motion
+                    :initial="{ opacity: 0, y: 400}"
+                    :enter="{ opacity: 1, y: 0, transition: { duration: 400, delay: 500 } }" class="card card_padding flex-1 flex justify-end items-center mb-4 sm:mb-0 sm:mr-4">
                     <div class="flex-1 relative flex items-end">
-                        
                         <!-- PRESET EXISTS -->
                         <div v-if="store.currentDiceSet != 0">
                             <NuxtLink :to="{ name: 'Roll'}" class="flex flex-col items-center z-50" tabindex=-1>
@@ -48,12 +78,12 @@ definePageMeta({
                             </NuxtLink>
 
                             <div class="relative flex items-center">
-                                <NuxtLink :to="{ name: 'Roll'}" class="z-50 btnapp btn_large uppercase hover:scale-105 transition cursor-pointer textInGroupTransitionFix">
+                                <NuxtLink :to="{ name: 'Roll'}" class="z-50 btnapp btn_small uppercase hover:scale-105 transition cursor-pointer textInGroupTransitionFix">
                                     Roll Preset
                                 </NuxtLink>
                                 <!-- TODO: v-motion conflicts with hover transform... we want both. How to make it work? -->
                                 <!-- v-motion-slide-left :delay="700" -->
-                                <div v-motion-slide-left class="btn_large w-full -right-10 sm:-right-18 absolute z-0" style="padding-right: 1em">
+                                <div v-motion-slide-left class="btn_small w-full -right-10 sm:-right-18 absolute z-0" style="padding-right: 1em">
                                     <NuxtLink v-if="store.currentDiceSet != 0" :to="{ name: 'Sets'}"
                                     class="w-full flex-1 ml-4 flex justify-end rounded-full bg-[color:var(--color-primary)] bg-opacity-10 group hover:translate-x-2 transition-transform"
                                     style="padding: 0.8em">
@@ -73,7 +103,7 @@ definePageMeta({
                                 </div>
                             </NuxtLink>
 
-                            <NuxtLink :to="{ name: 'Sets'}" class="btnapp btn_large text-center hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
+                            <NuxtLink :to="{ name: 'Sets'}" class="btnapp btn_small text-center hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
                                 Roll Preset
                             </NuxtLink>
                         </div>
@@ -85,18 +115,15 @@ definePageMeta({
                     </NuxtLink>
                     -->
                 </section>
-            </div>
 
-
-
-            <div class="w-full flex flex-col sm:flex-row sm:justify-center">
-                <!-- LEFT --> 
-                <section v-motion-slide-bottom :delay="700" class="flex-1 card card_padding mb-4 sm:mb-0 sm:mr-4 flex justify-center items-center overflow-hidden">
+                <section v-motion
+                    :initial="{ opacity: 0, y: 400}"
+                    :enter="{ opacity: 1, y: 0, transition: { duration: 400, delay: 400 } }" class="flex-1 card card_padding flex justify-center items-center overflow-hidden">
                     <!-- TODO: Ideally we want to use a named route with params, but apparently that is ill-advised, and now throws an error (from VueRouter) -->
                     <!-- <NuxtLink :to="{ name: 'Roll', params: { 'set': 'classic' } }" tabindex=-1 class="flex flex-col items-center"> -->
                     <!-- SEE: https://github.com/vuejs/router/blob/main/packages/router/CHANGELOG.md#414-2022-08-22 -->
                     <!-- FOR NOW: This works, we go to a raw path: -->
-                    <NuxtLink :to="{ name: 'Sets'}" class="flex-1 flex flex-col justify-end items-center" tabindex=-1>
+                    <NuxtLink :to="{ name: 'Sets'}" class="flex-1 flex items-center" tabindex=-1>
                         <div class="mb-4 flex transition hover:scale-105 space-x-6 p-6">
 
                             <div class="flex items-end pr-6 border-r border-[color:var(--color-primary)]">
@@ -132,17 +159,7 @@ definePageMeta({
                         Browse Dice Sets
                     </NuxtLink>
                 </section>
-                <!-- RIGHT -->
-                <section v-motion-slide-bottom :delay="1900" class="flex-1 card card_padding flex justify-center items-center">
-                    <NuxtLink :to="{ name: 'Sets'}" class="flex-1 flex flex-col justify-end items-center" tabindex=-1>
-                        <div class="mb-2 flex justify-center transition hover:scale-105 space-x-4 p-4">
-                            <IconsBase name="pencil" class="w-20 h-auto" />
-                        </div>
-                    </NuxtLink>
-                    <NuxtLink :to="{ name: 'Sets'}" class="btnapp btn_small hover:scale-105 transition cursor-pointer uppercase whitespace-nowrap textInGroupTransitionFix">
-                        Create Custom Dice
-                    </NuxtLink>
-                </section>
+
             </div>
 
 
